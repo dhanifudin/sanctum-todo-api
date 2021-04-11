8b2fc7cfef7fb29b4401bc4a181ac4278a139ef5
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-04-12 06:29:40 +0700

    init project

---
+++ app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+
+class TodoController extends Controller
+{
+
+    use ApiResponse;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::with('user')->get();
+
+        return $this->apiSuccess($todos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TodoRequest $request)
+    {
+        $request->validated();
+
+        $user = auth()->user();
+        $todo = new Todo($request->all());
+        $todo->user()->associate($user);
+        $todo->save();
+
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TodoRequest $request, Todo $todo)
+    {
+        $request->validated();
+        $todo->save();
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            return $this->apiSuccess($todo->delete());
+        }
+        return $this->apiError('Unauthorized', JsonResponse::HTTP_UNAUTHORIZED);
+    }
+}
